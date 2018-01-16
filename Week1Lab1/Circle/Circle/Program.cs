965074ad5a96ac4207f7a1d3ebdd09e79bec077b
Raiymbek <rzhainazar@gmail.com>
2018-01-17 01:36:01 +0500

    two changes

---
+++ Week1Lab1/Circle/Circle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Circle
+    {
+        public int r;
+        public double area;
+        public int diameter;
+
+        public Circle()
+        {
+            r = 1;
+        }
+
+        public Circle(int a)
+        {
+            r = a;
+        }
+
+        public void FindArea()
+        {
+            area = Math.PI * r * r;
+        }
+
+        public void FindDiameter()
+        {
+            diameter = r * 2;
+        }
+
+        public override string ToString()
+        {
+            return "Area:" + area + ' ' + "Diameter:" + diameter;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            Circle c = new Circle(k);
+
+            c.FindArea();
+            c.FindDiameter();
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week1Lab1/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        public int width;
+        public int length;
+        public int area;
+        public int per;
+
+        public Rectangle()
+        {
+            width = 0;
+            length = 0;
+        }
+        public Rectangle(int a, int b)
+        {
+            width = b;
+            length = a;
+        }
+
+        public void FindArea()
+        {
+            area = length * width;
+        }
+
+        public void FindPerim()
+        {
+            per = 2 * (length + width);
+        }
+
+        public override string ToString()
+        {
+            return "Area:" + area + ' ' + "Perimeter:" + per;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] par = s.Split(' ');
+            int a = int.Parse(par[0]);
+            int b = int.Parse(par[1]);
+
+            Rectangle r = new Rectangle(a, b);
+
+            r.FindArea();
+            r.FindPerim();
+
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
