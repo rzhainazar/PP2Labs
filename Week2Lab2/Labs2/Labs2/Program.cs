a08ed426ad14ba62cf2f1058bc368fa012c71d04
Raiymbek <rzhainazar@gmail.com>
2018-01-17 10:43:43 +0500

    Labs2

---
+++ Week2Lab2/Labs2/Labs2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string s = Console.ReadLine();
+            string[] ss = s.Split(' ');
+            int min = int.Parse(ss[0]);
+            int max = int.Parse(ss[1]);
+            foreach(string t in ss)
+            {
+                min = Math.Min(min, int.Parse(t));
+                max = Math.Max(max, int.Parse(t));
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week2Lab2/Labs2/Labs2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Labs2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Labs2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6561135a-1f17-4e89-a380-3be3abad952d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
