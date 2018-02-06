5d030d74b6938ea9c26d7480858469fa7ebf657d
Raiymbek <rzhainazar@gmail.com>
2018-02-07 00:35:42 +0500

    asd

---
+++ Week3_Labs3/Week3_Labs3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Labs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
